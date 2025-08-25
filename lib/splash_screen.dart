e5412d7ec5d89139ea450921c940c310fe5580b2
hnnayy <mutiahanin2017@gmail.com>
2025-08-25 23:14:46 +0700

    add home page hardcode

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,32 +1,45 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Color(0xFF125E72),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/logo/logo.png',
-                width: 250,
-                height: 250,
-              ),
-              Text(
-                'PLN JagaGRID',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFFFEE62E),
-                  fontFamily: 'Poppins',
+      body: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const HomePage()),
+          );
+        },
+        child: Container(
+          color: const Color(0xFF125E72),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logo/logo.png',
+                  width: 250,
+                  height: 250,
                 ),
-              ),
-            ],
+                const Text(
+                  'PLN JagaGRID',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFFFEE62E),
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
